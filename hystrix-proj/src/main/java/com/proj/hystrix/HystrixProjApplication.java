fbd3ce14d11a5556d7b284e14b31637fe7c6e5fb
damod <damod@LAPTOP-J5TN0NTC.broadband>
2020-05-12 20:05:12 +0100

    First commit

---
+++ hystrix-proj/src/main/java/com/proj/hystrix/HystrixProjApplication.java
@@ -0,0 +1,13 @@
+package com.proj.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixProjApplication.class, args);
+	}
+
+}
---
+++ hystrix-proj/src/test/java/com/proj/hystrix/HystrixProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
